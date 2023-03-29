bc2b0012ddf47c163dae8e2c666b268031aa654d
matumoto-yu <songbenyuzai@gmail.com>
2023-03-30 02:09:31 +0900

    view and chat resize ok at maxview; *yet seekbar resize / view resize after koukoku / chat userName hide

--- js/live-view.js
+++ js/live-view.js
@@ -1,6 +1,42 @@
 
-var video_object = document.getElementsByTagName("video");
-video_object[0].classList.add("view");
+
+var video_object = document.getElementsByTagName("video")[0];
+var screen_size = screen.width;
+
+video_size_change()
+
+window.addEventListener("resize", video_size_change());
 
 var bottom_bar = document.getElementsByClassName("ytp-chrome-bottom");
 bottom_bar[0].classList.add("bottom-bar");
+
+
+
+function video_size_change() {
+    
+    if (window.outerWidth === screen_size) {
+        // video_object[0].classList.add("video-view");
+        // video_object.style.width = "872px";
+        video_object.style.width = "100%";
+        video_object.style.height = "516px";
+    }
+
+}
+
+
+
+
+
+
+
+
+
+// var video_style = "--ytd-watch-flexy-scrollbar-width:17px; --ytd-watch-flexy-space-below-player:31px; --ytd-watch-flexy-panel-max-height:385px; --ytd-watch-flexy-chat-max-height:385px; --ytd-watch-flexy-structured-description-max-height:385px; --ytd-watch-flexy-comments-panel-max-height:385px;";
+// window.onload = function() {
+//     console.log("hello");
+//     setTimeout(function(){
+//         // console.log("I am the third log after 5 seconds");
+//         document.getElementsByTagName("ytd-watch-flexy")[0].style = video_style;
+//     },10000);
+    
+// }
