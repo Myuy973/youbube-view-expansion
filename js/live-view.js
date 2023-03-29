d207b5f69c741c701a6a63fd156705d7a642e77d
matumoto-yu <songbenyuzai@gmail.com>
2023-03-29 17:30:48 +0900

    first file commit

---
+++ js/live-view.js
@@ -0,0 +1,6 @@
+
+var video_object = document.getElementsByTagName("video");
+video_object[0].classList.add("view");
+
+var bottom_bar = document.getElementsByClassName("ytp-chrome-bottom");
+bottom_bar[0].classList.add("bottom-bar");
