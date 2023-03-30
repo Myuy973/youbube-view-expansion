5fefed86c721962de8de80581f8261c185c059d3
matumoto-yu <songbenyuzai@gmail.com>
2023-03-30 19:40:45 +0900

    min player -> defult player: fix video resize error

--- js/live-view.js
+++ js/live-view.js
@@ -1,40 +1,48 @@
 
 
-var video_object = document.getElementsByTagName("video")[0];
-var screen_size = screen.width;
+window.onload = function() {
+    var video_object = document.getElementById("player").getElementsByTagName("video")[0];
+    var screen_size = screen.width;
+    var watch_url_pattern = "https://www.youtube.com/watch?v=";
+    
+    video_size_change()
+    
+    window.addEventListener("resize", video_size_change());
+    
+    
+    var video_attributes_check = new MutationObserver(() => {
+        video_object = document.getElementById("player").getElementsByTagName("video")[0];
+        video_size_change();
+    });
+    var video_config = { attributes: true };
+    video_attributes_check.observe(video_object, video_config);
+    
 
-video_size_change()
-
-window.addEventListener("resize", video_size_change());
-
-
-var video_attributes_check = new MutationObserver(() => {
-    video_size_change();
-});
-var config = { attributes: true };
-video_attributes_check.observe(video_object, config);
-  
+    var url_config = { childList: true, subtree: true };
+    video_attributes_check.observe(document, url_config);
 
 
-var bottom_bar = document.getElementsByClassName("ytp-chrome-bottom");
-bottom_bar[0].classList.add("bottom-bar");
+    var bottom_bar = document.getElementsByClassName("ytp-chrome-bottom");
+    bottom_bar[0].classList.add("bottom-bar");
 
 
 
-function video_size_change() {
-    // console.log("video_size_change start");
-    if (window.outerWidth === screen_size) {
-        // video_object[0].classList.add("video-view");
-        // video_object.style.width = "872px";
-        // console.log("video_size_change check ok");
-        video_object.style.width = "100%";
-        video_object.style.height = "516px";
+    function video_size_change() {
+        // console.log("video_size_change start: " + location.href.includes(watch_url_pattern));
+        if ((window.outerWidth === screen_size || 
+            location.href.includes(watch_url_pattern)) &&
+            video_object) {
+            // console.log("video_size_change check ok");
+            // console.log("url " + location.href);
+            // console.log("video_object " + video_object);
+            video_object.style.width = "100%";
+            video_object.style.height = "516px";
+        }
+
     }
 
+
 }
-
-
-
 
 
 
